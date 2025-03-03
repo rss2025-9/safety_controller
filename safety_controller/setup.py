05a8fbbe1aaee925ca5a5883a8eefcae7bd02d35
selinnal <selinnalin0705@gmail.com>
2025-03-03 22:23:37 +0000

    node name?

--- safety_controller/setup.py
+++ safety_controller/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'safety_controller = safety_controller.safety_controller:main',
+            'safety_controller = safety_controller.SafetyController:main',
         ],
     },
 )
