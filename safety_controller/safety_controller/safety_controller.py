3411f89b198a0a19a302ceb9e75181253e2ff841
hu-tree <huarthur2@gmail.com>
2025-03-05 17:27:08 -0500

    Modified to include speed based stopping threshold

--- safety_controller/safety_controller/safety_controller.py
+++ safety_controller/safety_controller/safety_controller.py
@@ -12,44 +12,44 @@ class SafetyController(Node):
         super().__init__("safety_controller")
 
         self.stop_thresh=0.5  #meters before stopping
-        self.stop_speed=0.0  #stopping speed
+        self.stop_speed = 5  # stopping speed
+        self.current_speed = 0.0
 
-        #declare ROS params
+        # declare ROS params
         self.declare_parameter("use_real_racecar", True)  #for real car vs. sim
         self.declare_parameter("drive_topic", "/drive")  #default for simulation
         self.declare_parameter("safety_topic", "/vesc/low_level/input/safety")  #for real car
 
-        #get params
+        # get params
         self.use_real_racecar=self.get_parameter("use_real_racecar").get_parameter_value().bool_value
         self.DRIVE_TOPIC=self.get_parameter("drive_topic").get_parameter_value().string_value
         self.SAFETY_TOPIC=self.get_parameter("safety_topic").get_parameter_value().string_value
 
-        #based on environ
+        # based on environ
         self.output_topic=self.SAFETY_TOPIC if self.use_real_racecar else self.DRIVE_TOPIC
 
-        #pub, as said in prompt
+        # pub, as said in prompt
         self.drive_pub=self.create_publisher(AckermannDriveStamped, self.output_topic, 10)
 
-        #sub, as said in prompt
+        # sub, as said in prompt
         self.scan_sub=self.create_subscription(LaserScan, "/scan", self.scan_callback, 10)
         self.ackermann_sub=self.create_subscription(AckermannDriveStamped, "/vesc/low_level/ackermann_cmd", self.ackermann_callback, 10)
 
-
     def scan_callback(self, msg):
-        #LIDAR to detenct potential collisions
+        # LIDAR to detenct potential collisions
 
-        #LIDAR to np array
+        # LIDAR to np array
         ranges=np.array(msg.ranges)
         angles=np.linspace(msg.angle_min, msg.angle_max, len(ranges))
 
-        #only -pi/3 to pi/3
+        # only -pi/3 to pi/3
         mask=(angles>=-np.pi/3)&(angles<=np.pi/3)
         good_range=ranges[mask]
 
-        #bad data out
+        # bad data out
         good_range=good_range[(good_range>msg.range_min)&(good_range<msg.range_max)]
 
-        #closest thing to hit
+        # closest thing to hit
         if len(good_range)>0:
             closest_pt=np.min(good_range)
         else:
@@ -57,17 +57,18 @@ class SafetyController(Node):
 
         self.get_logger().info(f"Closest point: {closest_pt:.3f}m")  #to debug
 
-        #if closer than threshold, stop
-        if closest_pt<self.stop_thresh:
+        # if closer than threshold, stop
+        if closest_pt < max(self.stop_thresh, self.current_speed**2 / self.stop_speed):
             self.get_logger().warn("Publishing stop command")
             self.publish_stop_command()
 
     def ackermann_callback(self, msg):
-        #intercepts driving commant
+        # intercepts driving commant
         self.get_logger().info(f"Received Drive Command: Speed={msg.drive.speed}, Steering={msg.drive.steering_angle}")
+        self.current_speed = msg.drive.speed
 
     def publish_stop_command(self):
-       #pub stop command
+        # pub stop command
         stop_msg=AckermannDriveStamped()
         stop_msg.drive.speed=self.stop_speed
         stop_msg.drive.steering_angle=0.0
@@ -86,5 +87,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
